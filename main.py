5bb5e6fdeb8ee9f578ce1e3214c98247b01e504e
jakeiridion <emil.ej.jeleskovic@gmail.com>
2021-11-04 10:43:50 +0100

    better typing

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from typing import List
 import matplotlib.pyplot as plt
 
 
+Individual = List[int]
+Population = List[Individual]
+
+
 class GeneticAlgorithm:
     def __init__(self):
         self._population_size: int = 10
@@ -14,10 +18,10 @@ class GeneticAlgorithm:
         self._mutation_probability: float = 0.01
         self._number_of_iterations: int = 100
 
-    def create_initial_population(self) -> List:
-        return [numpy.random.randint(0, 2, 6).tolist() for _ in range(self._population_size)]
+    def create_initial_population(self) -> Population:
+        return [list(numpy.random.randint(0, 2, 6)) for _ in range(self._population_size)]
 
-    def tournament_selection(self, population: List) -> List:
+    def tournament_selection(self, population: Population) -> Individual:
         best = self._get_random_individual(population)[:]
         for _ in range(self._tournament_size-1):
             challenger = self._get_random_individual(population)[:]
@@ -25,11 +29,11 @@ class GeneticAlgorithm:
                 best = challenger[:]
         return best
 
-    def calculate_fitness(self, individual: List) -> float:
+    def calculate_fitness(self, individual: Individual) -> float:
         x = int("".join(str(digit) for digit in individual), 2)
         return self._fitness_function(x)
 
-    def one_point_crossover(self, parent1: List, parent2: List) -> List:
+    def one_point_crossover(self, parent1: Individual, parent2: Individual) -> Population:
         if random.random() < self._crossover_probability:
             crossover_point = random.randint(1, len(parent1)-1)
             partial_p1 = parent1[:crossover_point]
@@ -38,13 +42,13 @@ class GeneticAlgorithm:
             parent2[:crossover_point] = partial_p1
         return [parent1, parent2]
 
-    def flip_bit_mutation(self, individual: List) -> List:
+    def flip_bit_mutation(self, individual: Individual) -> Individual:
         for i in range(len(individual)-1):
             if random.random() < self._mutation_probability:
                 individual[i] = 1 - individual[i]
         return individual
 
-    def _get_random_individual(self, population: List) -> List:
+    def _get_random_individual(self, population: Population) -> Individual:
         return population[random.randint(0, len(population)-1)]
 
     def run(self) -> int:
