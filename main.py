3727d33597d4fb732e81d4f0b8ccff99bfdf0a5a
jakeiridion <emil.ej.jeleskovic@gmail.com>
2021-11-04 15:29:48 +0100

    removed unneeded brackets and changed population size to 8.

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ Population = List[Individual]
 
 class GeneticAlgorithm:
     def __init__(self):
-        self._population_size: int = 10
+        self._population_size: int = 8
         self._tournament_size: int = 2
         self._fitness_function = lambda x: (-math.pow(x, 2) / 21) + 3 * x
         self._crossover_probability: float = 0.90
         self._mutation_probability: float = 0.01
-        self._number_of_iterations: int = 100
+        self._number_of_iterations: int = 50
 
     def create_initial_population(self) -> Population:
         return [list(numpy.random.randint(0, 2, 6)) for _ in range(self._population_size)]
 
     def tournament_selection(self, population: Population) -> Individual:
-        best = self._get_random_individual(population)[:]
+        best = self._get_random_individual(population)
         for _ in range(self._tournament_size-1):
             challenger = self._get_random_individual(population)[:]
             if self.calculate_fitness(challenger) > self.calculate_fitness(best):
@@ -53,22 +53,22 @@ class GeneticAlgorithm:
 
     def run(self) -> int:
         self._setup_graph()
-        population = self.create_initial_population()[:]
-        best = self._get_random_individual(population)[:]
+        population = self.create_initial_population()
+        best = self._get_random_individual(population)
         for _ in range(self._number_of_iterations):
             for individual in population:
                 self._plot_dot(individual)
                 if self.calculate_fitness(individual) > self.calculate_fitness(best):
                     best = individual[:]
             # Tournament Selection:
-            population = [self.tournament_selection(population) for _ in range(self._population_size)][:]
+            population = [self.tournament_selection(population) for _ in range(self._population_size)]
             # One Point Crossover:
             crossed_over_population = []
             for _ in range(int(self._population_size/2)):
                 crossed_over_population += self.one_point_crossover(self._get_random_individual(population),
                                                                     self._get_random_individual(population))
             # Flip Bit Mutation:
-            population = [self.flip_bit_mutation(individual) for individual in crossed_over_population][:]
+            population = [self.flip_bit_mutation(individual) for individual in crossed_over_population]
         self._show_graph()
         return int("".join(str(digit) for digit in best), 2)
 
